c8ffe60014959da1d0f3488520c89241f7506f3f
Teppei Koshiji <Coolandhotman@outlook.jp>
2023-10-15 20:32:16 +0900

    ** web.phpのroute表記変更 ** 表記方法が古かったので新しい表記に変更

--- routes/web.php
+++ routes/web.php
@@ -12,48 +12,47 @@
 // | be assigned to the "web" middleware group. Make something great!
 // |
 // */
+use App\Http\Controllers\BlogController;
 
 # ブログ一覧を表示
-// 一番最初に開いた画面で表示
-Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
+# 一番最初に開いた画面で表示
+Route::get('/', [BlogController::class, 'showList'])->name('blogs');
 # キーワード検索
-Route::get('/search', 'App\Http\Controllers\BlogController@search')->name('search');
+Route::get('/search', [BlogController::class, 'search'])->name('search');
 # ログインユーザーが宿題を投稿した記事のみ表示
-Route::get('/userSearch', 'App\Http\Controllers\BlogController@userSearch')->name('userSearch');
+Route::get('/userSearch', [BlogController::class, 'userSearch'])->name('userSearch');
 # 全ユーザーが宿題を投稿した記事を表示
-Route::get('/allSearch', 'App\Http\Controllers\BlogController@allSearch')->name('allSearch');
+Route::get('/allSearch', [BlogController::class, 'allSearch'])->name('allSearch');
 
-# 新規宿題登録画面を表示
-Route::get('/blog/create', 'App\Http\Controllers\BlogController@showCreate')->name('create');
-# スレッドを新規登録
-Route::post('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
+# /blogから始まるURLをグループ化
+Route::prefix('blog')->group(function () {
+    # 新規宿題登録画面を表示
+    Route::get('/create', [BlogController::class, 'showCreate'])->name('create');
+    # スレッドを新規登録
+    Route::post('/store', [BlogController::class, 'exeStore'])->name('store');
 
-# 新規アカウント登録画面表示
-Route::get('/blog/signUp','App\Http\Controllers\BlogController@showSignUp')->name('signUp');
+    # 新規アカウント登録画面表示
+    Route::get('/signUp', [BlogController::class, 'showSignUp'])->name('signUp');
+    # 新規アカウント登録実行
+    Route::post('/registration', [BlogController::class, 'exeRegistration'])->name('registration');
 
-# 新規アカウント登録実行
-Route::post('/blog/registration', 'App\Http\Controllers\BlogController@exeRegistration')->name('registration');
+    # ログイン画面表示
+    Route::get('/login', [BlogController::class, 'showLogin'])->name('login');
+    # ログイン実行
+    Route::post('/exelogin', [BlogController::class, 'exeLogin'])->name('exelogin');
+    # ログアウト実行
+    Route::get('/logout', [BlogController::class, 'Logout'])->name('logout');
 
-# ログイン画面表示
-Route::get('/blog/login','App\Http\Controllers\BlogController@showLogin')->name('login');
-
-# ログイン実行
-Route::post('/blog/exelogin','App\Http\Controllers\BlogController@exeLogin')->name('exelogin');
-
-# ログアウト実行
-Route::get('/blog/logout','App\Http\Controllers\BlogController@Logout')->name('logout');
-
-# 宿題詳細画面表示
-Route::get('/blog/{id}', 'App\Http\Controllers\BlogController@showDetail')->name('show');
-# 回答の送信
-Route::post('/blog/reply', 'App\Http\Controllers\BlogController@exeReply')->name('reply');
-# 問題解決したことを反映
-Route::post('/blog/resolve', 'App\Http\Controllers\BlogController@exeResolve')->name('resolve');
-
-# 編集画面を表示
-Route::get('/blog/edit/{id}', 'App\Http\Controllers\BlogController@showEdit')->name('edit');
-# 編集内容に更新処理を実行
-Route::post('/blog/update', 'App\Http\Controllers\BlogController@exeUpdate')->name('update');
-
-# 投稿した宿題の削除
-Route::post('/blog/delete/{id}', 'App\Http\Controllers\BlogController@exeDelete')->name('delete');
+    # 宿題詳細画面表示
+    Route::get('/{id}', [BlogController::class, 'showDetail'])->name('show');
+    # 回答の送信
+    Route::post('/reply', [BlogController::class, 'exeReply'])->name('reply');
+    # 問題解決したことを反映
+    Route::post('/resolve', [BlogController::class, 'exeResolve'])->name('resolve');
+    # 編集画面を表示
+    Route::get('/edit/{id}', [BlogController::class, 'showEdit'])->name('edit');
+    # 編集内容に更新処理を実行
+    Route::post('/update', [BlogController::class, 'exeUpdate'])->name('update');
+    # 投稿した宿題の削除
+    Route::post('/delete/{id}', [BlogController::class, 'exeDelete'])->name('delete');
+});
